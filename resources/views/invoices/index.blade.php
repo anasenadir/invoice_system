0bd5ad46ae6e726e5949d7005f30f36659fd2d43
anasenadire <enadireahmed@gmail.com>
2023-01-21 22:54:21 +0000

    show all the invoices

--- resources/views/invoices/index.blade.php
+++ resources/views/invoices/index.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
+@section('css')
+    <style>
+        svg{
+            width: 20px !important;
+        }
+
+        span[aria-current] > span{
+            background: #0d6efd !important;
+            color: white
+        }
+    </style>
+@endsection
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -13,6 +25,43 @@
                             Create Invoice
                         </button>
                     </a>
+
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Customer Name</th>
+                                    <th>Email</th>
+                                    <th>Invoice Number</th>
+                                    <th>Phone</th>
+                                    <th>Total Due</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($invoices as $invoice)
+                                    <tr>
+                                        <td><a href="{{ route('invoice.show', $invoice->id) }}">{{$invoice->customer_name}}</a> </td>
+                                        <td>{{$invoice->customer_email}}</td>
+                                        <td>{{$invoice->invoice_number}}</td>
+                                        <td>{{$invoice->invoice_date}}</td>
+                                        <td>{{$invoice->total_due}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                            <tfoot>
+                                
+                                {{-- {{$invoices->setCursorName()}} --}}
+                            </tfoot>
+                        </table>
+
+
+                        <div class="">
+                            {{$invoices->links()}}
+                            {{-- {{$invoices->nextPageUrl()}} --}}
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
